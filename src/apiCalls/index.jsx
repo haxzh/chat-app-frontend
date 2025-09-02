366a65c850566db7697b832a909b78c843e50e70
Harshakya <168418718+haxzh@users.noreply.github.com>
2025-09-02 14:35:17 +0530

    upadate client

--- src/apiCalls/index.jsx
+++ src/apiCalls/index.jsx
@@ -6,13 +6,37 @@
 //     }
 // });
 
+
+
+
+
 import axios from "axios";
 
 export const url = "https://chat-app-backend-1-llty.onrender.com";
-// export const localUrl = "http://localhost:5000/";
 
 export const axiosInstance = axios.create({
+    baseURL: url, // <-- Yeh line add karo!
     headers: {
         authorization: `Bearer ${localStorage.getItem('token')}`
     }
 });
+
+
+
+
+
+
+
+
+
+
+// import axios from "axios";
+
+// export const url = "https://chat-app-backend-1-llty.onrender.com";
+// // export const localUrl = "http://localhost:5000/";
+
+// export const axiosInstance = axios.create({
+//     headers: {
+//         authorization: `Bearer ${localStorage.getItem('token')}`
+//     }
+// });
